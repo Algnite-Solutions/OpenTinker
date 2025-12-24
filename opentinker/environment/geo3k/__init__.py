868f5d72c13a5db92a5035ad4b3fb1396d34d19b
zhusq20 <siqizhu4@illinois.edu>
2025-12-24 00:46:28 +0000

    add geo3k qwen2.5-3b-vl example

---
+++ opentinker/environment/geo3k/__init__.py
@@ -0,0 +1,6 @@
+"""Geo3K geometry problem-solving game for OpenTinker."""
+
+from .geo3k_game import Geo3KGame
+from .geo3k_env import Geo3KGameEnvironment
+
+__all__ = ["Geo3KGame", "Geo3KGameEnvironment"]
